1e794a52a8038fa5c73f298daeeedbe3101087dc
Anushtha Dhaka <anushtha.dhaka@gmail.com>
2026-07-28 16:06:00 +0530

    commit

--- server/Server.js
+++ server/Server.js
@@ -4,26 +4,22 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import connectDB from './configs/Mongodb.js'
 import userRouter from './routes/userRouter.js'
+
 // App Config
 const PORT = process.env.PORT || 4000
 const app = express()
-await  connectDB()
+await connectDB()
+
 // Middleware
 app.use(express.json())
 app.use(cors())
 
-// // Connect to MongoDB
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI)
-//     console.log("Database Connected")
-//   } catch (error) {
-//     console.log(error.message)
-//   }
-// }
-// connectDB()
-
 // API routes
 app.get('/', (req, res) => res.send("API Working"))
-app.use('/api/user',userRouter)
- app.listen(PORT, () => console.log("Server running on port " + PORT))
+app.use('/api/user', userRouter)
+
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => console.log("Server running on port " + PORT))
+}
+
+export default app
