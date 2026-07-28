a9426afcdde5e883f2126ed1bdf394a05cbb626b
Anushtha Dhaka <anushtha.dhaka@gmail.com>
2026-07-28 19:12:30 +0530

    new edit

--- server/Server.js
+++ server/Server.js
@@ -1,23 +1,16 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
 import connectDB from './configs/Mongodb.js'
-import userRouter from './routes/userRouter.js'
-
-// App Config
+//api config
 const PORT = process.env.PORT || 4000
 const app = express()
 await connectDB()
-
-// Middleware
+//initialize middlewares
 app.use(express.json())
 app.use(cors())
 
-// API routes
-app.get('/', (req, res) => res.send("API Working"))
-app.use('/api/user', userRouter)
+//API routes
+app.get('/',(req,res)=>res.send("API working"))
 
-app.listen(PORT, () => console.log("Server running on port " + PORT))
-
-export default app
+app.listen(PORT,()=>console.log("Server running on port" +PORT))
